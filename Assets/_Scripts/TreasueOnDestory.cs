f0ebfdbf224df966e68a7b55c8d1cc0c87e28c45
MasterKrepta <brianj.pajak@gmail.com>
2021-07-16 23:20:19 -0400

    Level tweaks

--- Assets/_Scripts/TreasueOnDestory.cs
+++ Assets/_Scripts/TreasueOnDestory.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class TreasueOnDestory : MonoBehaviour
 {
     public GameObject[] treasures;
-    public float AmountToSpawn = 5;
+    public float AmountToSpawn;
     Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        //AmountToSpawn = Random.Range(0, 3);
+        AmountToSpawn = Random.Range(0, 3);
         rb = GetComponent<Rigidbody2D>();
     }
 
